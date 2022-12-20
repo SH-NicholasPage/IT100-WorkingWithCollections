540c484a3a6e2cc70ccaaf64859930988791d463
Nicholas Page <npage@southhills.edu>
2022-12-19 21:09:57 -0500

    Initial commit

---
+++ IT100-WorkingWithCollections/IT100-WorkingWithCollections/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+namespace IT100_WorkingWithCollections
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            UseArray();
+            UseArrayList();
+            UseList();
+            //TODO: Add benchmark runner here later
+        }
+
+        public static void UseArray()
+        { 
+            //TODO: Implement this
+        }
+
+        public static void UseArrayList()
+        {
+            //TODO: Implement this
+        }
+
+        public static void UseList()
+        {
+            //TODO: Implement this
+        }
+    }
+
+    public class MyBenchmark
+    {
+        //TODO: Implement this
+    }
+}
